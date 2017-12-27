7c21001daca6110453118fbee651d812be1c88b9
pierangelo <pierangelo1982@gmail.com>
2017-12-27 10:08:49 +0100

    listview 2

--- 17ListView2/app/src/main/java/com/orizio/pierangelo/a17listview2/MainActivity.java
+++ 17ListView2/app/src/main/java/com/orizio/pierangelo/a17listview2/MainActivity.java
@@ -2,6 +2,15 @@ package com.orizio.pierangelo.a17listview2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView cousinsListView = (ListView)findViewById(R.id.cousinsListView);
+
+        final ArrayList<String> myCousins = new ArrayList<String>(asList("Monica", "Bruno", "Maddalena", "Raffaele", "Gioele"));
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, myCousins);
+
+        cousinsListView.setAdapter(arrayAdapter);
+
+        cousinsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(getApplicationContext(), "Ciao " + myCousins.get(position), Toast.LENGTH_SHORT).show();
+                
+            }
+        });
+
     }
 }
