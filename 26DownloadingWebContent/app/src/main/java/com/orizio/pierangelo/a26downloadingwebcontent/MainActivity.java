20ba707b0cd5aa7fff5b4d6907b8966e2a5ae132
pierangelo <pierangelo1982@gmail.com>
2017-12-31 18:05:22 +0100

    downloading web cointent

---
+++ 26DownloadingWebContent/app/src/main/java/com/orizio/pierangelo/a26downloadingwebcontent/MainActivity.java
@@ -0,0 +1,83 @@
+package com.orizio.pierangelo.a26downloadingwebcontent;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+
+                url = new URL(params[0]);
+
+                urlConnection = (HttpURLConnection)url.openConnection();
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+
+                while (data != -1) {
+
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = inputStreamReader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+                return "Failed";
+            }
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask downloadTask = new DownloadTask();
+        String result = null;
+
+        try {
+
+            result = downloadTask.execute("https://www.ecowebhosting.co.uk/").get();
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        } catch (ExecutionException e) {
+
+            e.printStackTrace();
+
+        }
+
+        Log.i("Contents Of URL", result);
+
+    }
+}
